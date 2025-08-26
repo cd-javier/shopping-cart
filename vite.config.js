ddf66c20a51e9052eec8ee5b09a291cd98930fdf
Javier Quiroga <contact@javierquiroga.com>
2025-08-26 21:46:00 +0100

    feat: Set up react and vite environment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/shopping-cart',
   plugins: [react()],
+  server: {
+    host: true,
+  },
 });
