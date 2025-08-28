08e6338a78d8ea33c1bd5de3ae4ac52c21eacd80
Javier Quiroga <contact@javierquiroga.com>
2025-08-28 17:41:10 +0100

    feat: add signature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,25 @@ export default function Footer() {
         © 2025 <span style={{ fontStyle: 'italic' }}>things!</span> All rights
         reserved. Probably.
       </div>
+      <div className={styles.signature}>
+        Designed and developed by{' '}
+        <a
+          href="http://www.javierquiroga.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Visit my portfolio"
+        >
+          Javier Quiroga
+        </a>{' '}
+        - Check it out on{' '}
+        <a
+          href="https://github.com/cd-javier/shopping-cart"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
     </footer>
   );
 }
